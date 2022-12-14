fc24acaa9e8367624fc73c32b9e85d01755488e9
Oliver Alt <oa@lacheweg21.de>
2022-12-14 15:01:02 +0100

    Initial code commit

---
+++ src/MDD4All.FileAccess.Contracts/IFileSaver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDD4All.FileAccess.Contracts
+{
+    public interface IFileSaver
+    {
+        bool ShowFileSaveDialog(out string selectedFilename, string defaultFielname = "",
+                                string defaultFileExtension = "",
+                                string filter = "All Files (*.*)|*.*");
+
+        void WriteDataToFile(string filename, byte[] data);
+    }
+}
